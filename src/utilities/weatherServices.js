c60eadecfe49db9c782c16d350a8469a07f9c495
Shaik-Riyaz-Ziegler <ShaikriyazZiegleruk@outlook.com>
2024-01-20 21:31:02 +0530

    made more secure by adding api key

--- src/utilities/weatherServices.js
+++ src/utilities/weatherServices.js
@@ -1,40 +1,41 @@
 import axios from "axios";
 
-
-const API_KEY = "691022e047d6e88d4866d7b9bdf500c6";
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 
 const makeIconURL = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`;
 
 const getFormattedWeatherData = async (city, units = "metric") => {
-  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
-  try{
-    const data = await axios.get(URL)
-      .then((res) => res.data)
-      const {
-        weather,
-        main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
-        wind: { speed },
-        sys: { country },
-        name,
-      } = data;
-      const { description, icon } = weather[0];
-      
-      return {
-        description,
-        iconURL : makeIconURL(icon),
-        temp,
-        feels_like,
-        temp_min,
-        temp_max,
-        pressure,
-        humidity,
-        speed,
-        country,
-        name,
-      };
-    }catch(e){
-      throw new Error("Not Found");
-    }
+  try {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+    const { data } = await axios.get(URL);
+
+    const {
+      weather,
+      main: { temp, feels_like, temp_min, temp_max, pressure, humidity },
+      wind: { speed },
+      sys: { country },
+      name,
+    } = data;
+
+    const { description, icon } = weather[0];
+
+    return {
+      description,
+      iconURL: makeIconURL(icon),
+      temp,
+      feels_like,
+      temp_min,
+      temp_max,
+      pressure,
+      humidity,
+      speed,
+      country,
+      name,
+    };
+  } catch (error) {
+    throw new Error("Not Found");
+  }
 };
 
 export { getFormattedWeatherData };
